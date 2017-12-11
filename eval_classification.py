e40d15dee4de2ebfca6970cca85fedea213d8f68
Demetrios Fassois <dimitris.fasois@gmail.com>
2017-12-10 21:22:17 -0600

    Add files via upload

--- eval_classification.py
+++ eval_classification.py
@@ -1,9 +1,15 @@
+
 import os
 import glob
 import pandas as pd
 from sklearn import linear_model
+import numpy as np
 
 from util import *
+
+os.chdir("C:\\Users\\fade7001\\Documents\\Resources\\CS229\\CS229 Project")
+
+songs = pd.read_csv("A_N_lda.csv")
 
 # evaluates our algorithm against a particular playlist
 playlist_song_titles = {}
@@ -15,49 +21,53 @@ with open('./playlists/60s, 70s, 80s Classic Rock.txt', 'r') as inFile:
 playlist_songs = {}
 neg_examples = [] # random selection of negative examples
 i = 0
-for subset_file in msd:
-    print subset_file
-    songs = pd.read_csv(subset_file)
-    for index, _ in songs.iterrows():
-        song = songs.iloc[index]
 
-        if not ast.literal_eval(song['audio_features'])[0]:
-            continue
+for index, _ in songs.iterrows():
+    song = songs.iloc[index]
 
-        key = song_key(song['artist_name'],song['title'])
+    if not ast.literal_eval(song['audio_features'])[0]:
+        continue
 
-        if key in playlist_song_titles:
-            playlist_songs[key] = (flatten_song(song))
-        elif i < 100:
-            i = i + 1
-            neg_examples.append(flatten_song(song))
+    key = song['song_artist_title'] 
 
-playlist = playlist_songs.values()
+    if key in playlist_song_titles:
+        playlist_songs[key] = (flatten_song(song))
+    elif i < 100:
+        i = i + 1
+        neg_examples.append(flatten_song(song))
+
+playlist = list(playlist_songs.values())
 playlist_len = len(playlist)
-divider = playlist_len * 3 / 4
-print 'We have ' + str(playlist_len) + ' songs'
+mid = playlist_len * 3 / 4
+print( 'We have ' + str(playlist_len) + ' songs')
 
-pos_train = playlist[0:divider] # 3/4 of playlist_songs
-neg_train = neg_examples[0:divider] # random songs not in playlist
+pos_train = playlist[0:int(mid)] # 3/4 of playlist_songs
+neg_train = neg_examples[0:int(mid)] # random songs not in playlist
 
-pos_test = playlist[divider:playlist_len] # other 1/4 of playlist_songs
-neg_test = neg_examples[divider:playlist_len] # random songs not in playlist
+pos_test = playlist[int(mid):playlist_len] # other 1/4 of playlist_songs
+neg_test = neg_examples[int(mid):playlist_len] # random songs not in playlist
 
 x_train = pos_train + neg_train
 y_train = [ 1 for x in range(len(pos_train))] + [ 0 for x in range(len(neg_train))]
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+x_test = pos_test + neg_test
+y_test = [ 1 for x in range(len(pos_test))] + [ 0 for x in range(len(neg_test))]
+
+x_test = np.array(x_test)
+y_test = np.array(y_test)
 
 model = linear_model.LogisticRegression(C=1e5)
 
 model.fit(x_train, y_train)
 
-pos_pred_train = model.predict(pos_train)
-print 'Train accuracy'
-print round(float(sum(pos_pred_train)) / float(len(pos_pred_train)), 2)
+train_preds = model.predict(x_train)
+print('Train accuracy')
+print((train_preds == y_train).sum()/y_train.shape[0])
+print(round(float(sum(train_preds)) / float(len(x_train)), 2))
 
-pos_pred = model.predict(pos_test)
-print 'Test accuracy'
-print round(float(sum(pos_pred)) / float(len(pos_pred)), 2)
-
-neg_pred = model.predict(neg_test)
-print 'Percent false positive'
-print round(float(sum(neg_pred)) / float(len(neg_pred)), 2)
+test_preds = model.predict(x_test)
+print('Test accuracy')
+print((test_preds == y_test).sum()/y_test.shape[0])
